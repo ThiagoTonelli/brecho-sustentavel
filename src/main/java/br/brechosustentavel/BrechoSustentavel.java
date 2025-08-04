012c00b254c87b500c624cd99676e53323b0af52
Thiago Tonelli da Silva <thiagktonelli@gmail.com>
2025-08-04 14:10:04 -0300

    feat: adiciona o padrão de projeto MVP

---
+++ src/main/java/br/brechosustentavel/BrechoSustentavel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.brechosustentavel;
+
+/**
+ *
+ * @author thiag
+ */
+public class BrechoSustentavel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
