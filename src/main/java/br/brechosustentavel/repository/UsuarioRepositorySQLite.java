78c928822211a7ed8ebf99cade47c97785e30399
Thiago Tonelli da Silva <thiagktonelli@gmail.com>
2025-08-08 16:53:17 -0300

    Criando factory para repositórios e conexões

--- src/main/java/br/brechosustentavel/repository/UsuarioRepositorySQLite.java
+++ src/main/java/br/brechosustentavel/repository/UsuarioRepositorySQLite.java
@@ -4,18 +4,39 @@
  */
 package br.brechosustentavel.repository;
 
+import br.brechosustentavel.model.Usuario;
 import java.sql.Connection;
+import java.util.Optional;
 
 /**
  *
  * @author thiag
  */
-public class UsuarioRepositorySQLite {
+public class UsuarioRepositorySQLite implements IUsuarioRepository{
     private Connection conexao;
 
     public UsuarioRepositorySQLite(Connection conexao) {
         this.conexao = conexao;
     }
-    
+
+    @Override
+    public void criar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void excluir() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void editar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Optional<Usuario> consultar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
