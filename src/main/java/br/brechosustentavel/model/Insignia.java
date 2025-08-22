54a3be0f966a216331d4bec91a17b893fd4e4e54
kailany <kailanyalves04@gmail.com>
2025-08-22 16:44:38 -0300

    adiciona classe model Insignia

--- src/main/java/br/brechosustentavel/model/Insignia.java
+++ src/main/java/br/brechosustentavel/model/Insignia.java
@@ -9,5 +9,46 @@ package br.brechosustentavel.model;
  * @author kaila
  */
 public class Insignia {
+    private int id;
+    private String nome;
+    private double valorEstrelas;
+    private String tipoPerfil;
     
+    public Insignia(String nome, double valorEstrelas, String tipoPerfil){
+        this.nome = nome;
+        this.valorEstrelas = valorEstrelas;
+        this.tipoPerfil = tipoPerfil;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValorEstrelas() {
+        return valorEstrelas;
+    }
+
+    public void setValorEstrelas(double valorEstrelas) {
+        this.valorEstrelas = valorEstrelas;
+    }
+
+    public String getTipoPerfil() {
+        return tipoPerfil;
+    }
+
+    public void setTipoPerfil(String tipoPerfil) {
+        this.tipoPerfil = tipoPerfil;
+    }   
 }
